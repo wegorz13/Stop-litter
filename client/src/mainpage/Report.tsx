b413ce73d35398ae5e808a834199b3ef9afe8d2c
Filip Mokrzycki <filipmok@student.agh.edu.pl>
2025-03-29 14:31:47 +0100

    Front-end refactor

--- client/src/mainpage/Report.tsx
+++ client/src/mainpage/Report.tsx
@@ -1,16 +1,26 @@
 import ReportInterface from "./ReportInterface";
 
-import './Report.css'; // Import the CSS file
 
 export default function Report({ report }: { report: ReportInterface }) {
   return (
-    <div className="report-card">
-      <img src="src/assets/smieci-1.png" alt="Logo" height="100px" width="100px" />
-      <div className="text-container">
-        <h1>{report.title}</h1>
-        <h2>{report.date}</h2>
-        <h2>{report.address}</h2>
+    <div className="px-4 py-4">
+      <div className="row border">
+        <div className="col-5 px-0">
+          <img src="src/assets/smieci-1.png" alt="Logo" width="100%"/>
+        </div>
+        <div className="col-7 text-center mt-4">
+          <h1>{report.title}</h1>
+          <h2>{report.date}</h2>
+          <h2>{report.address}</h2>
+          <div>
+            <div className="justify-content-end">
+              <button className="btn btn-success fs-3">Bump</button>
+              <h3 className="">Bumps:</h3>
+            </div>
+          </div>
+        </div>
       </div>
+      
     </div>
   );
 }
--- client/src/mainpage/ReportsList.tsx
+++ client/src/mainpage/ReportsList.tsx
@@ -3,10 +3,10 @@ import Report from "./Report";
 export default function ReportsList({reports}:{reports:ReportInterface[]}){
     return (
         <>
-      <div >
+      <div className="row align-items-start ">
         {reports.map((report, index) => {
           return (
-            <div key={index}>
+            <div key={index} className="col-6">
               <Report report={report} />
             </div>
           );
