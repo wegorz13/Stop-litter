be2487e1f8775eb70455b83597023e3da708db18
Filip Mokrzycki <filipmok@student.agh.edu.pl>
2025-03-29 15:44:19 +0100

    reports

--- client/src/mainpage/Report.tsx
+++ client/src/mainpage/Report.tsx
@@ -2,25 +2,34 @@ import ReportInterface from "./ReportInterface";
 
 
 export default function Report({ report }: { report: ReportInterface }) {
+  function addLike(): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <div className="px-4 py-4">
       <div className="row border">
         <div className="col-5 px-0">
-          <img src="src/assets/smieci-1.png" alt="Logo" width="100%"/>
+          <img src="src/assets/smieci-1.png" alt="Logo" width="100%" />
         </div>
-        <div className="col-7 text-center mt-4">
-          <h1>{report.title}</h1>
-          <h2>{report.date}</h2>
-          <h2>{report.address}</h2>
+        <div className="col-7 text-center d-flex flex-column justify-content-between">
           <div>
-            <div className="justify-content-end">
-              <button className="btn btn-success fs-3">Bump</button>
-              <h3 className="">Bumps:</h3>
+            <h2 className="text-start text-secondary">{report.date}</h2>
+            <h1 className="f">{report.title}</h1>
+            <h2 className="fs-2 text-start fw-normal mt-3">{report.description}</h2>
+          </div>
+          <div className="mt-auto row">
+            <div className="col mt-auto">
+              <h2 className="fs-5">{report.address}</h2>
+            </div>
+            <div className="col">
+              <h3>5</h3>
+              <button className="btn btn-success fs-3" onClick={()=> addLike()}>Bump</button>
             </div>
           </div>
         </div>
       </div>
-      
-    </div>
+  </div>
   );
 }
+
--- client/src/mainpage/ReportsList.tsx
+++ client/src/mainpage/ReportsList.tsx
@@ -1,18 +1,47 @@
 import ReportInterface from "./ReportInterface";
 import Report from "./Report";
-export default function ReportsList({reports}:{reports:ReportInterface[]}){
-    return (
-        <>
-      <div className="row align-items-start ">
-        {reports.map((report, index) => {
-          return (
-            <div key={index} className="col-6">
-              <Report report={report} />
-            </div>
-          );
-        })}
-      </div>
+import { useEffect, useState } from "react";
 
+export default function ReportsList({ reports }: { reports: ReportInterface[] }) {
+  const [category, setCategory] = useState<string>("date");
+  const [sortedReports, setSortedReports] = useState<ReportInterface[]>([...reports]);
+
+  function sortReports(category: string) {
+    let sorted = [...reports];
+
+    if (category === "date") {
+      sorted.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    }
+    // } else if (category === "bumps") {
+    //   sorted.sort((a, b) => b.bumps - a.bumps);
+    // }
+
+    setSortedReports(sorted);
+  }
+
+  const onCategoryChange = (category: string) => {
+    setCategory(category);
+    sortReports(category);
+  };
+  useEffect(()=>{
+    sortReports('date')
+  },[])
+
+  return (
+    <>
+    <div className="text-center fs-2">
+      <select className="text-center" value={category} onChange={(e) => onCategoryChange(e.target.value)}>
+        <option value="date">Date</option>
+        <option value="bumps">Bumps</option>
+      </select>
+    </div>
+      <div className="row align-items-start">
+        {sortedReports.map((report, index) => (
+          <div key={index} className="col-6">
+            <Report report={report} />
+          </div>
+        ))}
+      </div>
     </>
-    )
+  );
 }
