8a94ebe1be54a1fb6aaf5c139b891e1c7e851288
Filip <filipwegrzyn85@gmail.com>
2025-04-12 20:21:30 +0200

    add navbar css

--- client/src/mainpage/Navbar.tsx
+++ client/src/mainpage/Navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div
+      {/* <div
         className="collapse navbar-collapse fs-4"
         id="navbarSupportedContent"
       >
@@ -23,7 +23,55 @@ function Navbar() {
           </li>
         </ul>
       </div>
-      <div className="logo ml-auto">stop-litter</div>
+      <div className="logo ml-auto">stop-litter</div> */}
+
+      <div className="container-fluid custom-light-blue mx-2">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse fs-4"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+            <li className="nav-item ">
+              <a
+                className="nav-link active text-secondary text-dark link"
+                aria-current="page"
+                href="/"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item ">
+              <a
+                className="nav-link active text-secondary text-dark link"
+                aria-current="page"
+                href="/form"
+              >
+                Contribute
+              </a>
+            </li>
+            <li className="nav-item ">
+              <a
+                className="nav-link active text-secondary text-dark link"
+                href="/charts"
+              >
+                Charts
+              </a>
+            </li>
+          </ul>
+        </div>
+        <p className="navbar-brand logo ml-auto">stop-litter</p>
+      </div>
     </nav>
   );
 }
